90d50636c678307d18d7ae077e342fa4bdb6097e
JasonDoe <43284628+Thafoxes@users.noreply.github.com>
2025-05-08 15:31:01 +0800

    added icons

---
+++ core/src/main/java/io/github/Eco_warrior/constant/ConstantVar.java
@@ -0,0 +1,9 @@
+package io.github.Eco_warrior.constant;
+
+class ConstantsVar {
+
+    public static final int WINDOW_HEIGHT = 720;
+    public static final int WINDOW_WIDTH = 1280;
+    public static final String WINDOW_TITLE = "Eco Warrior";
+
+}
---
+++ core/src/main/java/io/github/Eco_warrior/entity/shovel.java
@@ -0,0 +1,5 @@
+package io.github.Eco_warrior.entity;
+
+public class shovel {
+
+}
