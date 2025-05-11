f01160a12f32c81983248c37b9fa4ef88e6861a7
JasonW <43284628+Thafoxes@users.noreply.github.com>
2025-05-12 00:58:33 +0800

    Added level 2 map and load it to the java

--- core/src/main/java/io/github/eco_warrior/LevelTwoScreen.java
+++ core/src/main/java/io/github/eco_warrior/LevelTwoScreen.java
@@ -1,10 +1,39 @@
 package io.github.eco_warrior;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL32;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import static com.badlogic.gdx.Gdx.gl;
+import static io.github.eco_warrior.constant.ConstantsVar.WINDOW_HEIGHT;
+import static io.github.eco_warrior.constant.ConstantsVar.WINDOW_WIDTH;
 
 public class LevelTwoScreen implements Screen {
 
+
     private Main game;
+
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private SpriteBatch batch;
+    private SpriteBatch uiBatch;
+
+    //map
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer maprenderer;
+
+
+    //delta time
+    private float stateTime;
+
+
     public LevelTwoScreen(Main main) {
         this.game = main;
 
@@ -12,17 +41,42 @@ public class LevelTwoScreen implements Screen {
 
     @Override
     public void show() {
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(WINDOW_WIDTH, WINDOW_HEIGHT, camera);
+        batch = new SpriteBatch();
+        uiBatch = new SpriteBatch();
 
+        //setup camera to middle
+        camera.position.set(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0);
+        camera.update();
+
+        loadMap();
+    }
+
+    private void loadMap(){
+        TmxMapLoader.Parameters parameters = new TmxMapLoader.Parameters();
+        parameters.generateMipMaps = false;
+        map = new TmxMapLoader().load("maps/soil_map.tmx", parameters);
+
+        maprenderer = new OrthogonalTiledMapRenderer(map, 1f);
     }
 
     @Override
     public void render(float delta) {
+        draw();
+    }
 
+    private void draw() {
+        camera.update();
+        gl.glClear(GL32.GL_COLOR_BUFFER_BIT);
+
+        maprenderer.setView(camera);
+        maprenderer.render();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -42,6 +96,9 @@ public class LevelTwoScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        uiBatch.dispose();
+        batch.dispose();
+        maprenderer.dispose();
+        map.dispose();
     }
 }
