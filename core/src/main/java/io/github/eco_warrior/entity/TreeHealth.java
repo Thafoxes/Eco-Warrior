1c8e0b0a7a72259d4ee78edbe8b92a96b6cc3a8e
WawaChan2 <wahhinchiwawa@gmail.com>
2025-06-16 09:50:23 +0800

    yes

--- core/src/main/java/io/github/eco_warrior/entity/TreeHealth.java
+++ core/src/main/java/io/github/eco_warrior/entity/TreeHealth.java
@@ -4,14 +4,6 @@ import com.badlogic.gdx.math.Vector2;
 
 public class TreeHealth extends gameSprite {
 
-    public enum TreeHealthAnimation {
-        HP_MAX,
-        HP_3,
-        HP_2,
-        HP_1,
-        HP_0
-    }
-
     Trees tree;
 
     public TreeHealth(String atlasPath, String regionBaseName, Vector2 position, Trees tree) {
@@ -25,22 +17,7 @@ public class TreeHealth extends gameSprite {
     }
 
     public void updateHealth() {
-        switch (tree.health) {
-            case 4:
-                setFrame(TreeHealthAnimation.HP_MAX.ordinal());
-                break;
-            case 3:
-                setFrame(TreeHealthAnimation.HP_3.ordinal());
-                break;
-            case 2:
-                setFrame(TreeHealthAnimation.HP_2.ordinal());
-                break;
-            case 1:
-                setFrame(TreeHealthAnimation.HP_1.ordinal());
-                break;
-            default:
-                setFrame(TreeHealthAnimation.HP_0.ordinal());
-                break;
-        }
+        int frameIndex = Math.max(0, Math.min(4, 4 - tree.health));
+        setFrame(frameIndex);
     }
 }
